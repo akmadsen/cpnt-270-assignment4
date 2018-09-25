21e072113cf9fe4ab47f9e1b2c5c93b0ad006211
Alexander Madsen <akmadsen522@gmail.com>
2018-09-25 11:16:17 -0600

    Mobile Nav now opens and closes

--- js/app.js
+++ js/app.js
@@ -2,16 +2,22 @@
 // 1) Set-up 
 // * * * * * * * * * * * * * * * * * * * * * * * *
 let openButton = document.querySelector("#open-button"); 
+let closeButton = document.querySelector("#close-button"); 
 let mobileMenu = document.querySelector("#mobile-menu"); 
 
 // * * * * * * * * * * * * * * * * * * * * * * * *
 // 2) Write Functions 
 // * * * * * * * * * * * * * * * * * * * * * * * *
 function open() { 
-    console.log("Open Pressed!"); 
+    mobileMenu.className = "mobile-menu"; 
+}
+
+function close() { 
+    mobileMenu.className = "mobile-menu closed"; 
 }
 
 // * * * * * * * * * * * * * * * * * * * * * * * *
 // 3) Wire it All Together
 // * * * * * * * * * * * * * * * * * * * * * * * *
 openButton.addEventListener('click', open); 
+closeButton.addEventListener('click', close); 
