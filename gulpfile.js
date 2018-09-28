d9b4416e9a041c88a5c9e5ff783d11f5edfac8fc
Alex Madsen <akmadsen522@gmail.com>
2018-09-27 19:59:31 -0600

    Set up SASS plugins - working so far

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass'); 
+
+gulp.task('style', function() {
+    gulp.src('./sass/app.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./public/css'));
+}); 
+
+gulp.task('watch', function() {
+    // Any folder or file with .scss, run the 'style' task
+    gulp.watch('./src/sass/**/*.scss', ['style']); 
+}); 
